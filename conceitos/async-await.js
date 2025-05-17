ae78337aac0953edbb93a403a59b9f5732e89c15
nataliaado <nataliaado@hotmail.com>
2025-05-17 16:00:55 -0300

    novos conceitos de modularizacao

---
+++ conceitos/async-await.js
@@ -0,0 +1,10 @@
+
+
+// async - assíncrono
+async function exibirNomeDogFormatado (nome) {
+    return nome.toUpperCase()
+  }
+  
+  // await - esperar
+  console.log(await exibirNomeDogFormatado(`Pipoca`))
+  
